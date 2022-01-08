897c68bbca50402ef1762f0d588c9dd76ce58301
MyEternity (Home) <spddarkforce47@gmail.com>
2022-01-09 01:42:11 +0300

    l02 react + emulating json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UserList from "./components/users";
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        };
+    };
+
+    componentDidMount() {
+        const users = [
+            {
+                'username': 'admin',
+                'first_name': 'test',
+                'last_name': 'test',
+                'email': 'test@mail.local'
+            },
+            {
+                'username': 'demo',
+                'first_name': 'demo',
+                'last_name': 'test',
+                'email': 'demo@mail.loc'
+            }
+        ]
+        this.setState(
+            {
+                'users': users
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+        );
+    };
+};
 
 export default App;
